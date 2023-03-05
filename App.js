fe7ad3e7a97e20e438d5a60ea190ca0640100453
Luis <Luistejeda0011@gmail.com>
2023-03-05 14:15:16 -0400

    router added

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StyleSheet, View } from 'react-native';
-import Main from './src/components/Main'
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer,DefaultTheme } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import Main from './screens/Main'
+import Details from './screens/Details'
+
+const Stack = createStackNavigator()
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    backgroundColor: "transparent"
+  }
+}
 
 export default function App() {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if (!loaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Main />
-    </View>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+        <Stack.Screen name='Home' component={Main} />
+        <Stack.Screen name='Details' component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
