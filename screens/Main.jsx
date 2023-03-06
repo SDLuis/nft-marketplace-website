bd5b2aae2d42853441cdf49f9aa5bb1bf7bcacba
Luis <Luistejeda0011@gmail.com>
2023-03-06 14:37:08 -0400

    Render cards

--- screens/Main.jsx
+++ screens/Main.jsx
@@ -1,12 +1,54 @@
 import { useState } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
 import { COLORS, NFTData } from '../constants';
-import { HomeHeader, FocusedStatusBar, NFTCard } from '../components'
+import { HomeHeader, FocusedStatusBar, NFTCard } from '../components';
 export default function Main() {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (value.length === 0) {
+      setNftData(NFTData);
+    }
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length === 0) {
+      setNftData(NFTData);
+    } else {
+      setNftData(filteredData);
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar backgroundColor={COLORS.primary} />
-      
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={nftData}
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
+          />
+        </View>
+
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -17,4 +59,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
